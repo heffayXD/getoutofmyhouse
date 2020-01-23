0d9fef905305336206c25ca39f4b111681833ea6
Jeff <heffayXD@gmail.com>
2020-01-23 14:46:40 -0700

    Meta description

--- components/HeadComponent.js
+++ components/HeadComponent.js
@@ -5,6 +5,7 @@ const HeadComponent = props => {
     <Head>
       <title>{props.title || 'GetOutOfMyHouse'}</title>
       <link rel='shortcut icon' type='image/x-icon' href='/static/favicon.ico' />
+      <meta name='Description' content='GetOutOfMyHouse is a full stack development site that uses modern tech stacks to create a range of applications and websites.' />
       <link href='/static/css/fontawesome.min.css' rel='stylesheet' />
       <link href='/static/css/solid.min.css' rel='stylesheet' />
       <link href='/static/css/brands.min.css' rel='stylesheet' />

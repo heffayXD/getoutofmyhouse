fcc7133d5855510f061b2f28c8afa26004488851
Jeff <heffayXD@gmail.com>
2021-06-30 19:54:33 -0600

    Added static directory

--- next.config.js
+++ next.config.js
@@ -3,19 +3,6 @@ const withSass = require('@zeit/next-sass')
 const optimizedImages = require('next-optimized-images')
 const withCss = require('@zeit/next-css')
 
-// function removeMinimizeOptionFromCssLoaders (config) {
-//   console.warn('HACK: Removing `minimize` option from `css-loader` entries in Webpack config')
-//   config.module.rules.forEach(rule => {
-//     if (Array.isArray(rule.use)) {
-//       rule.use.forEach(u => {
-//         if (u.loader === 'css-loader' && u.options) {
-//           delete u.options.minimize
-//         }
-//       })
-//     }
-//   })
-// }
-
 module.exports = withPlugins([
   [optimizedImages],
   [withSass],
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   static async getInitialProps (ctx) {
@@ -8,13 +8,13 @@ class MyDocument extends Document {
 
   render () {
     return (
-      <html lang='en'>
+      <Html lang='en'>
         <Head />
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     )
   }
 }
